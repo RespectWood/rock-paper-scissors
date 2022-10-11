3116dba30b6f3f7ed8a0054c1b0956b1721a18e8
andre starnman <andre.starnman@gmail.com>
2022-10-11 16:20:11 +0200

    prettier extension functioncal

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,128 +1,120 @@
-
 // User input in Console (npm prompt-sync)
 
 const prompt = require("prompt-sync")();
 
 // Variables
 
-let computerSelection = ''
-const waitTime = 1000
-const empty = "                    "   
-
+let computerSelection = "";
+const waitTime = 1000;
+const empty = "                    ";
 
 // Score counters
 
-let tieCounter = 0
-let playerScore = 0
-let computerScore = 0
+let tieCounter = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 const Winner = {
   Tie: 1,
   Computer: 2,
-  Player: 3
-}
-
-
+  Player: 3,
+};
 
 // Capitalize first letter
 
 capitalize = (s) => {
-  return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase()
-}
+  return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
+};
 
+const hand = getUserChoice();
+const playerSelection = hand;
 
-const hand = getUserChoice()
-const playerSelection = hand
-
-
-
-function getUserChoice(){
+function getUserChoice() {
   const availableHands = ["Rock", "Paper", "Scissors"];
-  let isChoiceAllowed = false
-  while (!isChoiceAllowed){
-      let userChoice = prompt("please choose your hand : ")
-      userChoice = capitalize(userChoice)
-      
-      if (availableHands.includes(userChoice)){
-        return userChoice
-      }
-    }
+  let isChoiceAllowed = false;
+  while (!isChoiceAllowed) {
+    let userChoice = prompt("please choose your hand : ");
+    userChoice = capitalize(userChoice);
 
+    if (availableHands.includes(userChoice)) {
+      return userChoice;
+    }
+  }
 }
 
-
-// Get a random hand for the computer
+// Get a random hand for the computer - switch instead of if?
 
 function getComputerChoice() {
-  const num1 = Math.random() * 100
+  const num1 = Math.random() * 100;
 
   if (num1 <= 33) {
-    return 'Rock'
+    return "Rock";
   }
   if (num1 >= 33 && num1 <= 66) {
-    return 'Paper'
+    return "Paper";
   }
   if (num1 > 66) {
-    return 'Scissors'
+    return "Scissors";
   }
 }
-
 
 // Function for each round of play
 
-
-function playRound () {
-  computerSelection = getComputerChoice()
+function playRound() {
+  computerSelection = getComputerChoice();
 
   if (computerSelection === playerSelection) {
-    return Winner.Tie
+    return Winner.Tie;
   }
-  if (computerSelection === 'Rock' && playerSelection === 'Scissors') {
-    return Winner.Computer
-  }
-
-  if (computerSelection === 'Paper' && playerSelection === 'Rock') {
-    return Winner.Computer
+  if (computerSelection === "Rock" && playerSelection === "Scissors") {
+    return Winner.Computer;
   }
 
-  if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
-    return Winner.Computer
+  if (computerSelection === "Paper" && playerSelection === "Rock") {
+    return Winner.Computer;
+  }
+
+  if (computerSelection === "Scissors" && playerSelection === "Paper") {
+    return Winner.Computer;
   } else {
-    return Winner.Player
+    return Winner.Player;
   }
 }
 
-// colored text in console 
+// colored text in console
 const colors = require("colors");
 
-function playGame () {
+function playGame() {
   for (let i = 0; i < 10; i++) {
     // setTimeout (() => {
-    const winner = playRound()
+    const winner = playRound();
     if (winner === Winner.Tie) {
-      tieCounter += 1
-      console.log(`${empty}     Tie : ${tieCounter}`.cyan)
+      tieCounter += 1;
+      console.log(`${empty}     Tie : ${tieCounter}`.cyan);
     }
     if (winner === Winner.Computer) {
-      computerScore += 1
-      console.log(`Computer Score : ${computerScore}`.magenta)
+      computerScore += 1;
+      console.log(`Computer Score : ${computerScore}`.magenta);
     }
     if (winner === Winner.Player) {
-      playerScore += 1
-      console.log(`${empty}${empty}Player Score : ${playerScore}`.green)
+      playerScore += 1;
+      console.log(`${empty}${empty}Player Score : ${playerScore}`.green);
     }
-  // }, waitTime)
+    // }, waitTime + i)
+  }
 }
-}
 
-playGame()
+playGame();
 
-console.log("Calculating Scoresheet.....\n\n")
+console.log("Calculating Scoresheet.....\n\n");
 
-setTimeout (() => {
-  console.log("FINALSCORE FINALSCORE FINALSCORE FINALSCORE FINALSCORE ".rainbow)
-  console.log( { COMPUTER : computerScore, TIE : tieCounter, PLAYER : playerScore } )
-
-}, waitTime)
-
-
+setTimeout(() => {
+  console.log(
+    "FINALSCORE FINALSCORE FINALSCORE FINALSCORE FINALSCORE ".rainbow
+  );
+  console.log({
+    COMPUTER: computerScore,
+    TIE: tieCounter,
+    PLAYER: playerScore,
+  });
+}, waitTime);
