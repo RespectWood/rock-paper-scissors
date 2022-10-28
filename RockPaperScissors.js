fe4da7e1d77f64f2eb1f3c4788e87c7090220048
andre starnman <andre.starnman@gmail.com>
2022-10-28 16:10:11 +0200

    playerscore updating not logic

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,9 +1,3 @@
-// User input in Console (npm prompt-sync)
-
-// const prompt = require("prompt-sync")();
-
-// Variables
-
 const waitTime = 1000;
 
 const empty = " ";
@@ -20,35 +14,15 @@ const Winner = {
   Player: 3,
 };
 
-// Capitalize first letter
-// capitalize = (s) => {
-//   return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
-// };
-
-function getUserChoice() {
-  const availableHands = ["Rock", "Paper", "Scissors"];
-  let isChoiceAllowed = false;
-  while (!isChoiceAllowed) {
-    let userChoice = prompt("please choose your hand : ");
-    // userChoice = capitalize(userChoice);
-
-    if (availableHands.includes(userChoice)) {
-      return userChoice;
-    }
-  }
-}
-
 // Button
-
-function buttonchoice() {
-  let choiceButton = document.getElementById("buttons");
-
-  choiceButton.forEach((buttons) => {
-    buttons.addEventListener("click", () => {
-      console.log(buttons.id);
-    });
+const choiceButton = document.querySelectorAll(".selection-button");
+choiceButton.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    // console.log(e.target.id);
+    playRound(e.target.id);
   });
-}
+});
+
 // Get a random hand for the computer - switch instead of if?
 
 function getComputerChoice() {
@@ -67,59 +41,29 @@ function getComputerChoice() {
 
 // Function for each round of play
 
-function playRound() {
-  const playerSelection = getUserChoice();
+function playRound(playerSelection) {
   const computerSelection = getComputerChoice();
   console.log(`Computer choosed: ${computerSelection}`);
 
   if (computerSelection === playerSelection) {
-    return Winner.Tie;
+    tieCounter += 1;
   }
   if (computerSelection === "Rock" && playerSelection === "Scissors") {
-    return Winner.Computer;
+    computerScore += 1;
   }
 
   if (computerSelection === "Paper" && playerSelection === "Rock") {
-    return Winner.Computer;
+    computerScore += 1;
   }
 
   if (computerSelection === "Scissors" && playerSelection === "Paper") {
-    return Winner.Computer;
+    computerScore += 1;
   } else {
-    return Winner.Player;
+    playerScore += 1;
   }
-}
-
-// colored text in console
-// const colors = require("colors");
-
-function playGame() {
-  for (let i = 0; i < 1; i++) {
-    const winner = playRound();
-    if (winner === Winner.Tie) {
-      tieCounter += 1;
-      console.log(`${empty.repeat(25)}Tie : ${tieCounter}`);
-    }
-    if (winner === Winner.Computer) {
-      computerScore += 1;
-      console.log(`${empty.repeat(20)}Computer Score : ${computerScore}`);
-    }
-    if (winner === Winner.Player) {
-      playerScore += 1;
-      console.log(`${empty.repeat(20)}Player Score : ${playerScore}`);
-    }
-  }
-}
-
-playGame();
-
-console.log("Calculating Scoresheet.....\n\n");
-
-setTimeout(() => {
-  console.log(" FINALSCORE FINALSCORE FINALSCORE  ");
   console.log({
     COMPUTER: computerScore,
     TIE: tieCounter,
     PLAYER: playerScore,
   });
-}, waitTime);
+}
